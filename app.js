951378f42ae873a6e9611215f2b7bae6d4dd37b4
4A-H-M-E-D <santiagoforesta2021@gmail.com>
2024-02-06 08:49:26 +0200

    offcanvas-bootstrap

---
+++ app.js
@@ -0,0 +1,14 @@
+const cerrar = document.querySelector(".modificar");
+
+
+// const keyframes = [
+//     { transform: "rotate(360deg)" },
+//     { transform: "scale(0)"},
+//     { transform: "scale(1)"}
+// ]
+
+// cerrar.addEventListener("mouseover", () => {
+
+
+//     cerrar.animate(keyframes, 1000)
+// })
